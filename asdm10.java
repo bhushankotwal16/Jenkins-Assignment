09736391dc1e28278dea0471a263cc2e967a3694
bhushankotwal16 <bhushankotwal16@gmail.com>
2022-05-14 00:57:50 +0530

    done2

--- asdm10.java
+++ asdm10.java
@@ -2,5 +2,6 @@ class asdm10{
 public static void main(String[] args) {
 System.out.println("ASDM Jenkins Assignment");
 System.out.println("10th Assignment");
+System.out.println("b1 branch created ");
 }
 }
